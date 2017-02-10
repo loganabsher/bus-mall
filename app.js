e610333dc49b1150065a3234648bfb93f4500410
Logan Absher <absherlogan@gmail.com>
2017-02-10 12:56:31 -0800

    made functional program

--- app.js
+++ app.js
@@ -1,85 +1,124 @@
 'use strict';
-//creates a template for keeping track of data
-function image(picture, clicks, number){
+function Image(picture, name){
   this.picture = picture;
-  this.clicks = clicks;
-  this.id = number;
+  this.clicks = 0;
+  this.shown = 0;
+  this.name = name;
 }
-var bag = new image('img/bag.jpg', 0);
-var banana = new image('img/banana.jpg', 0);
-var bathroom = new image('img/bathroom.jpg', 0);
-var boots = new image('img/boots.jpg', 0);
-var breakfast = new image('img/breakfast.jpg', 0);
-var bubblegum = new image('img/bubblegum.jpg', 0);
-var chair = new image('img/chair.jpg', 0);
-var cthulhu = new image('img/cthulhu.jpg', 0);
-var dogDuck = new image('img/dog-duck.jpg', 0);
-var dragon = new image('img/dragon.jpg', 0);
-var pen = new image('img/pen.jpg', 0);
-var petSweep = new image('img/pet-sweep.jpg', 0);
-var scissors = new image('img/scissors.jpg', 0);
-var shark = new image('img/shark.jpg', 0);
-var sweep = new image('img/sweep.png', 0);
-var tauntaun = new image('img/tauntaun.jpg', 0);
-var unicorn = new image('img/unicorn.jpg', 0);
-var usb = new image('img/usb.gif', 0);
-var waterCan = new image('img/water-can.jpg', 0);
-var wineGlass = new image('img/wine-glass.jpg', 0);
-
-var imageArray = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, usb, waterCan, wineGlass];
-
-function randomImage(imageArray){
-  for(var i = 0; i < 3; i++){
-    var random = Math.round(Math.random() * 19);
-    console.log(random);
-    var gameEl = document.getElementById('imageGame');
-    var choiceEl = document.getElementById('choice');
-    var randomImg = document.createElement('img');
-    var buttonEl = document.createElement('button');
-    randomImg.setAttribute('class', 'medium');
-    randomImg.setAttribute('src', imageArray[random].picture);
-    buttonEl.setAttribute('type', 'submit');
-    buttonEl.setAttribute('id', (i + 1) + '_Image');
-    buttonEl.setAttribute('class', 'decision');
-    buttonEl.textContent = 'choice ' + (i + 1);
-    gameEl.appendChild(randomImg);
-    choiceEl.appendChild(buttonEl);
+var nameArray = [];
+nameArray.push(new Image('img/bag.jpg', 'bag'));
+nameArray.push(new Image('img/banana.jpg', 'banana'));
+nameArray.push(new Image('img/bathroom.jpg', 'bathroom'));
+nameArray.push(new Image('img/boots.jpg', 'boots'));
+nameArray.push(new Image('img/breakfast.jpg', 'breakfast'));
+nameArray.push(new Image('img/bubblegum.jpg', 'bubblegum'));
+nameArray.push(new Image('img/chair.jpg', 'chair'));
+nameArray.push(new Image('img/cthulhu.jpg', 'cthulhu'));
+nameArray.push(new Image('img/dog-duck.jpg', 'dogDuck'));
+nameArray.push(new Image('img/dragon.jpg', 'dragon'));
+nameArray.push(new Image('img/pen.jpg', 'pen'));
+nameArray.push(new Image('img/pet-sweep.jpg', 'petSweep'));
+nameArray.push(new Image('img/scissors.jpg', 'scissors'));
+nameArray.push(new Image('img/shark.jpg', 'shark'));
+nameArray.push(new Image('img/sweep.png', 'sweep'));
+nameArray.push(new Image('img/tauntaun.jpg', 'tauntaun'));
+nameArray.push(new Image('img/unicorn.jpg', 'unicorn'));
+nameArray.push(new Image('img/usb.gif', 'usb'));
+nameArray.push(new Image('img/water-can.jpg', 'waterCan'));
+nameArray.push(new Image('img/wine-glass.jpg', 'wineGlass'));
+function random(){
+  return Math.round(Math.random() * (nameArray.length - 1));
+}
+function randomControler(){
+  var one = random();
+  var two = random();
+  var three = random();
+  if(one === two){
+    two = random();
+  }
+  else if(one === three || two === three){
+    three = random();
+  }
+  return [one, two, three];
+}
+function renderImages(){
+  var choice = randomControler();
+  var gameEl = document.getElementById('image-game');
+  for(var i = 0; i < choice.length; i++){
+    var imageEl = document.createElement('img');
+    var getEl = nameArray[choice[i]];
+    imageEl.setAttribute('src', getEl.picture);
+    imageEl.setAttribute('id', getEl.name);
+    imageEl.setAttribute('class', 'medium');
+    gameEl.appendChild(imageEl);
+    getEl.shown++;
+  }
+  return choice;
+}
+function unrenderImages(choice){
+  var parentEl = document.getElementById('image-game');
+  for(var i = 0; i < choice.length; i++){
+    var childEl = document.getElementById(nameArray[choice[i]].name);
+    parentEl.removeChild(childEl);
   }
 }
-randomImage(imageArray);
-var choiceOne = document.getElementById('1_Image');
-var choiceTwo = document.getElementById('2_Image');
-var choiceThree = document.getElementById('3_Image');
-console.log(choiceThree);
-var imageChoice = function(event){
-  event.preventDefault();
-  event.stopPropagation();
-  console.log('sucess');
-};
-choiceOne.addEventListener('submit', imageChoice, false);
-// function condition(){
-//   var con = start();
-//   console.log(con);
-  // if(con === true){
-  //   do{
-  //     console.log('sucess');
-  //   }
-  //   while(con !== true);
-  // }
-// }
-// function start(){
-//   var formEl = document.getElementById('startForm');
-//   var startEl = document.createElement('button');
-//   startEl.setAttribute('type', 'submit');
-//   startEl.setAttribute('id', 'startButton');
-//   startEl.textContent = 'Start';
-//   formEl.appendChild(startEl);
-//   var startEvent = function(event){
-//     event.preventDefault();
-//     event.stopPropagation();
-//     formEl.remove(startEl);
-//     return true;
-//   };
-//   startEl.addEventListener('submit', startEvent, false);
-// }
-// condition();
+function createClickEvent(choice){
+  var firstEl = document.getElementById(nameArray[choice[0]].name);
+  var one = function(event){
+    nameArray[choice[0]].clicks++;
+    console.log('chosen first picture');
+    unrenderImages(choice);
+  };
+  var secondEl = document.getElementById(nameArray[choice[1]].name);
+  var two = function(event){
+    nameArray[choice[1]].clicks++;
+    console.log('chosen second picture');
+    unrenderImages(choice);
+  };
+  var thirdEl = document.getElementById(nameArray[choice[2]].name);
+  var three = function(event){
+    nameArray[choice[0]].clicks++;
+    console.log('chosen third picture');
+    unrenderImages(choice);
+  };
+  firstEl.addEventListener('click', one, false);
+  secondEl.addEventListener('click', two, false);
+  thirdEl.addEventListener('click', three, false);
+}
+function gameControler(){
+  var i = 0;
+  do{
+    var choice = renderImages();
+    console.log(choice);
+    createClickEvent(choice);
+    i++;
+  }
+  while(i < 3);
+}
+gameControler();
+// var context = document.getElementById('busmallChart').getContext('2d');
+// var chartArray = [100, 200, 300, 50, 10];
+// var chartColors = ['blue', 'red', 'yellow', 'orange', 'green'];
+// var chartOptions = {
+//   responsive: false,
+//   scales: {
+//     yAxes: [{
+//       ticks: {
+//         beginAtZero: true
+//       }
+//     }]
+//   }
+// };
+// var chartData = {
+//   labels: nameArray,
+//   datasets: [{
+//     label: '# of votes for each color',
+//     data: chartArray,
+//     backgroundColor: chartColors
+//   }]
+// };
+// var myFirstChart = new Chart(context, {
+//   type: 'bar',
+//   data: chartData,
+//   options: chartOptions
+// });
